4328b6cb7e027fc1af2856e97d77a6523ac8e593
DMGamage <gamagemadhushan@gmail.com>
2024-02-01 10:25:16 +0530

    Create pdf using excel data

--- main.py
+++ main.py
@@ -17,7 +17,26 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, text=f"invoice_nr: {invoice_nr}",ln=1)
 
     pdf.set_font("Times", size=16, style='B')
-    pdf.cell(w=50, h=8, text=f"Date: {date}")
+    pdf.cell(w=50, h=8, text=f"Date: {date}",ln=1)
+
+    columns = list(df.columns)
+    columns =[item.replace("_"," ") for item in columns]
+    pdf.set_font("Times", size=12)
+    pdf.set_text_color(00, 0, 0)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
+    pdf.cell(w=40, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1,ln=1)
+
+    for index ,row in df.iterrows():
+        pdf.set_font("Times", size=10)
+        pdf.set_text_color(80,80,80)
+        pdf.cell(w=30,h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70,h=8, txt=str(row["product_name"]),border=1)
+        pdf.cell(w=40,h=8, txt=str(row["amount_purchased"]),border=1)
+        pdf.cell(w=30,h=8, txt=str(row["price_per_unit"]),border=1)
+        pdf.cell(w=30,h=8, txt=str(row["total_price"]),border=1,ln=1)
 
     pdf.output(f"PDFs/{filename}.pdf", "F")
 
